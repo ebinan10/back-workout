0ac2c3f31bb0f342d25e316d89327a9ad6c59869
Olotu Ude Ebi Nan <ebinan10@gmail.com>
2023-06-13 20:37:43 +0100

    update to cors

--- App.js
+++ App.js
@@ -9,12 +9,23 @@ const WorkOutRouter = require('./router/Workout')
 const UserRouter = require('./router/User')
 const RefreshToken = require('./router/token')
 const cors = require('cors')
+
+
 app.use(express.json());   
 app.use(cors({
     origin: "https://frontend-9r12.onrender.com",
     credentials: true
 }
 ))
+let ALLOWED_ORIGINS = ["https://frontend-9r12.onrender.com"];
+app.use((req, res, next) => {
+    let origin = req.headers.origin;
+    let theOrigin = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
+    res.header("Access-Control-Allow-Origin", theOrigin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next();
+})
 app.options('*', cors())
       
 mongoose.connect(process.env.Uri)
